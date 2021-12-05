3eb18fa01ed9c6fe292ca34da6a8f5b8b1b2bcec
krupali02 <krupalipatel092@gmail.com>
2021-12-04 21:34:49 -0500

    new update

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function home () {
+    document.getElementById("testing").style.display= 'block'
+    document.getElementById("tony").style.display= 'none'
+    document.getElementById("hulk").style.display= 'none'
+
+}
+
+function a () {
+    document.getElementById("testing").style.display= 'none'
+    document.getElementById("tony").style.display= 'block'
+    document.getElementById("hulk").style.display= 'none'
+
+}
+
+function b () {
+    document.getElementById("testing").style.display= 'none'
+    document.getElementById("tony").style.display= 'none'
+    document.getElementById("hulk").style.display= 'block'
+
+}
